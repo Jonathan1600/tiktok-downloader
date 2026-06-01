0a8b788caa8933aabb3f2c9f296ed0b07a4d80b5
Jonathan <jonathancalderonsilberman@gmail.com>
2026-05-31 22:49:59 -0700

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,22 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Tiktok Downloader</title>
+        <meta name="description" content="Download Tiktok Videos" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+            Tiktok Downloader
+          </h1>
+          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
